936081a67ec82d9513668dcd786bc2a051ac9a4b
Doug Emery <emeryr@upenn.edu>
2014-10-07 08:19:58 -0400

    be smarter about getting secondary feasts

--- bin/get_saints.py
+++ bin/get_saints.py
@@ -4,21 +4,29 @@
 import re
 import sys
 
-MONTH_RE =  re.compile('((Jan|Feb|Mär|Apr|Mai|Jun|Jul|Aug|Sept|Oct|Nov|Dec)[.a-zA-Z]*\s+\d+):')
+MONTH_RE = re.compile('((Jan|Feb|Mär|Apr|Mai|Jun|Jul|Aug|Sept|Oct|Nov|Dec)[.a-zA-Z]*\s+\d+)(\s+\([^)]+\))?:')
 
-def print_days(saint, section):
-    if MONTH_RE.search(section):
-        for m in MONTH_RE.finditer(section):
-            print "%s\t%s\t%s" % (saint, m.group(1), section)
+def get_the_first_part(match,section):
+    return section[:match.start()]
+
+def print_days(saint, part, num):
+    if MONTH_RE.search(part):
+        for m in MONTH_RE.finditer(part):
+            if num > 0 and m.start() > 0:
+                print "%s\t%s\t%s\t%s" % (saint, get_the_first_part(m, part), m.group(1), part)
+            else:
+                print "%s\t%s\t%s\t%s" % (saint, '', m.group(1), part)
     else:
-        print "SKIPPED SECTION: %s" % section
+        print "SKIPPED SECTION: %s" % part
 
 for line in open(sys.argv[1]):
     parts =  line.strip().split('||')
     m = MONTH_RE.search(parts[0])
     if m:
-        saint = parts[0][:m.start()]
+        saint = get_the_first_part(m,parts[0])
+        i = 0
         for p in parts:
-            print_days(saint, p)
+            print_days(saint, p, i)
+            i += 1
     else:
         print "SKIPPED LINE: %s" % line.strip()
